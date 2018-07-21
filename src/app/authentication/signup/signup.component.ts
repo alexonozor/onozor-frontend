585e371ede846945c432d5da37a07d00c3efa485
charisol-oalex <alex.onozor@charisol.io>
2018-07-21 19:05:19 +0100

    feat(authentication) add signup

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { Angular2TokenService } from 'angular2-token';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +16,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
+  isSubmited: Boolean = false;
+  formErrors: Array<any>;
+  isThereError: Boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    public router: Router,
+    public _tokenService: Angular2TokenService,
+    private message: NzMessageService
+  ) {
   }
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      email: [null, [Validators.email]],
+      password: [null, [Validators.required]],
+      username:  [null, [Validators.required]],
+    });
+  }
+
+  signup() {
+    this.isSubmited = true;
+    this._tokenService.registerAccount(this.signupForm.value)
+      .subscribe(
+        res => {
+          if (localStorage.getItem('redirectTo')) {
+            this.router.navigateByUrl(localStorage.getItem('redirectTo'));
+          } else {
+            this.router.navigate(['/']);
+          }
+        },
+        err => {
+          this.isSubmited = false;
+          const error = err.json();
+          this.isThereError = true;
+          if (error) {
+            this.formErrors = error.errors.full_messages;
+          }
+        }
+      );
+  }
+
+  afterClose() {
+    this.isThereError = false;
+  }
+
 
 }
