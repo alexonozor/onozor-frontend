5e10a66d8de2c60f5614351b8120b195852d7cfd
alexonozor <alex.onozor@charisol.io>
2018-09-24 21:53:13 +0100

    fix(notifcation): add notification and search

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,7 +6,10 @@ import { AuthService } from '../../authentication/auth.service';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { CreatePostComponent } from '../../posts/create-post/create-post.component';
 import { NotificationService } from '../../notification/notification.service';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { switchMap, debounceTime, tap, finalize } from 'rxjs/operators';
+import { PostsService } from '../../posts/posts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -19,23 +22,34 @@ export class HeaderComponent implements OnInit {
   isLoging: any;
   notification_counter: number;
 
+  filteredUsers: any[] = [];
+  usersForm: FormGroup;
+  isLoading = false;
+  showSearch: false;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-  .pipe(
-    map(result => result.matches)
-  );
+    .pipe(
+      map(result => result.matches)
+    );
 
   isDesktop$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Web)
-  .pipe(
-    map(result => result.matches)
-  );
+    .pipe(
+      map(result => result.matches)
+    );
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     public auth: AuthService,
     public dialog: MatDialog,
-    public _notification: NotificationService
+    public _notification: NotificationService,
+    private fb: FormBuilder,
+    public _postService: PostsService,
+    public router: Router
   ) {
     this.auth.isCurrentUser();
+    this.usersForm = this.fb.group({
+      userInput: null
+    });
   }
 
   logout() {
@@ -45,6 +59,22 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.listenToCurrentUser();
+
+    this.usersForm
+      .get('userInput')
+      .valueChanges
+      .pipe(
+        debounceTime(300),
+        tap(() => this.isLoading = true),
+        switchMap(value => this._postService.search({ name: value }, 1)
+          .pipe(
+            finalize(() => this.isLoading = false),
+        )
+        )
+      )
+      .subscribe(users => {
+        this.filteredUsers = users.questions;
+      });
   }
 
   listenToNotificationUpdate() {
@@ -63,6 +93,18 @@ export class HeaderComponent implements OnInit {
       }
     });
     this.currentUser = this.auth.getCurrentUser();
+  }
+
+  goToPost(post) {
+    this.router.navigate(['posts', post.slug]);
+  }
+
+
+  showSearchForm(showSearch) {
+     this.showSearch = showSearch;
+     setTimeout(() => {
+      document.getElementById('searchForm').focus();
+     }, 100);
   }
 
 
